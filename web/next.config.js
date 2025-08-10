8931d9d7995eebab95a09797c703c2dde604659d
remrem1827 <lalremruatatlau0@gmail.com>
2025-08-10 17:43:37 +0530

    Remove experimental appDir setting and simplify Next.js configRemove experimental appDir setting and simplify Next.js configUpdate next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,14 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
-}
-
-module.exports = nextConfig
+module.exports = {}
